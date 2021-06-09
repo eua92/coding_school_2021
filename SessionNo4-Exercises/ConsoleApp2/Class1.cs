6cffbda34ea14f3f68c8ed9b0fb222516b130054
Eva <eua_92@hotmail.com>
2021-06-09 20:07:16 +0300

    Add project files.

---
+++ SessionNo4-Exercises/ConsoleApp2/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityLibrary
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        public virtual void GetName() { }
+
+        public virtual void SetName(string name) { }
+
+    }
+
+    public class Professor : Person
+    {
+        public string Rank { get; set; }
+        public List<Course> Courses { get; set; }
+
+        public void Teach(string course, DateTime datetime) { }
+        public void SetGrade(Guid studentId, Guid courseId, int grade) { }
+        public override void GetName() { }
+    }
+
+    public class Student : Person
+    {
+        public int RegistrationNumber { get; set; }
+        public List<Course> Courses { get; set; }
+        public void Attend(string course, DateTime dateTime) { }
+        public void WriteExam(string course, DateTime datetime) { }
+
+    }
+
+    public class Institute
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int YearsInService { get; set; }
+
+        public virtual void GetName() { }
+
+        public virtual void SetName(string name) { }
+    }
+
+    public class University : Institute
+    {
+        public List<Student> Students { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Grade> Grades { get; set; }
+        public List<Schedule> ScheduledCourses { get; set; }
+        public void GetStudents() { }       
+        public void GetCourses() { }
+        public void GetGrades() { }
+        public void SetSchedule(Guid courseId, Guid professorId, DateTime datetime ) { }
+
+    }
+
+    public class Grade
+    {
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid CourseId { get; set; }
+        public int GradeValue { get; set; }
+    }
+
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Subject { get; set; }
+    }
+
+    public class Schedule
+    {
+        public Guid Id { get; set; }
+        public Guid CourseId { get; set; }
+        public Guid ProfessorId { get; set; }
+        public DateTime CallendarId { get; set; }
+    }
+}
---
+++ SessionNo4-Exercises/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityLibrary;
+
+namespace ConsoleApp2 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+
+            //Question #1
+            Console.WriteLine("Enter a number");
+            decimal number = Convert.ToDecimal(Console.ReadLine());
+            ConvertToBinary(Convert.ToInt32(number));
+            Console.ReadLine();
+
+            //Question #2
+            Professor professor = new Professor();
+            professor.GetName();
+            Student student = new Student();
+            student.Attend("Math", new DateTime(2021, 06, 04, 10, 30, 0));
+        }
+
+        static int ConvertToBinary(int number)
+        {
+            int bin;
+            if (number != 0)
+            {              
+                bin = (number % 2) + 10 * ConvertToBinary(number / 2);
+                Console.Write(bin);
+                return 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
